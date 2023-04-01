1ecbf83fb66f53384d7903770e34642a18a070b6
Wenyong.He <Wenyong.He@justeattakeaway.com>
2023-04-01 10:28:15 -0400

    feat: improve cleanObject

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,6 @@
-export const isFalsy = (value) => (value === 0 ? false : !value);
-
-export const cleanObject = (object) => {
-    const result = { ...object };
-    Object.keys(result).forEach((key) => {
-        const value = result[key];
-        if (isFalsy(value)) {
-            delete result[key];
-        }
-    });
-    return result;
+export const cleanObject = (obj) => {
+    const filteredEntries = Object.entries(obj).filter(
+        ([_, value]) => value !== undefined && value !== '',
+    );
+    return Object.fromEntries(filteredEntries);
 };
