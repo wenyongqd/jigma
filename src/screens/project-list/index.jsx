5cddb294b1006a90d3a9d06f5f18fd22a87b6793
Wenyong.He <Wenyong.He@justeattakeaway.com>
2023-04-04 19:17:13 -0400

    feat: add debounce with custom hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,22 +12,23 @@ export const ProjectListScreen = () => {
         name: '',
         personId: '',
     });
+    const debouncedParam = useDebounce(param, 500)
 
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async (response) => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (response) => {
             if (response.ok) {
                 setList(await response.json());
             }
         });
-    }, [param]);
+    }, [debouncedParam]);
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async (response) => {
             if (response.ok) {
                 setUsers(await response.json());
             }
         });
-    }, []);
+    });
 
     return (
         <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+
 export const cleanObject = (obj) => {
     const filteredEntries = Object.entries(obj).filter(
         ([_, value]) => value !== undefined && value !== '',
     );
     return Object.fromEntries(filteredEntries);
+};
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback();
+    }, []);
+};
+//   0ms -----------> 200ms -----------> 400ms -----------> 900ms
+//  'a'               'ag'               'age'                |
+//   |                 |                   |                  |
+// (empty)       (cancel timeout#1)  (cancel timeout#2)    timeout#3
+// timeout#1         timeout#2           timeout#3
+
+// const debounce = (func, delay) => {
+//     let timeout;
+//     return (...param) => {
+//         if (timeout) {
+//             clearTimeout(timeout);
+//         }
+//         timeout = setTimeout(function() {
+//             func(...param);
+//         }, delay);
+//     }
+// }
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay);
+        return () => clearTimeout(timeout);
+    }, [value, delay]);
+
+    return debouncedValue;
 };
