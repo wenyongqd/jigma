47667a534c06bfe1210a3e53925d4b8920d79538
Wenyong.He <Wenyong.He@justeattakeaway.com>
2023-04-21 18:17:03 -0400

    feat: installed dev tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DevTools, loadServer } from 'jigma-dev-tool';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+loadServer(() =>
+    ReactDOM.render(
+        <React.StrictMode>
+            <DevTools />
+            <App />
+        </React.StrictMode>,
+        document.getElementById('root'),
+    ),
 );
 
 // If you want to start measuring performance in your app, pass a function
