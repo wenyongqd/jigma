839b74d016f8202cf21c8191fde218c622b691e6
wenyongqd <wenyongqd@gmail.com>
2023-06-07 14:45:16 -0400

    feat: optimize project list page layout with Grid and Flexbox

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,13 +2,64 @@ import React from 'react';
 import { ProjectListScreen } from 'screens/project-list';
 import { useAuth } from 'context/auth-context';
 import { Button } from 'antd';
+import styled from '@emotion/styled';
 
 export const AuthenticatedApp = () => {
     const { logout } = useAuth();
     return (
-        <div>
-            <Button onClick={logout}>Log out</Button>
-            <ProjectListScreen />
-        </div>
+        <Container>
+            <Header>
+                <HeaderLeft>
+                    <h3>Logo</h3>
+                    <h3>Projects</h3>
+                    <h3>User</h3>
+                </HeaderLeft>
+                <HeaderRight>
+                    <Button onClick={logout}>Log out</Button>
+                </HeaderRight>
+            </Header>
+            <Nav>nav</Nav>
+            <Main>
+                <ProjectListScreen />
+            </Main>
+            <Aside>aside</Aside>
+            <Footer>footer</Footer>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    display: grid;
+    grid-template-rows: 6rem 1fr 6rem;
+    grid-template-columns: 20rem 1fr 20rem;
+    grid-template-areas:
+        'header header header'
+        'nav main aside'
+        'footer footer footer';
+    height: 100vh;
+    grid-gap: 10rem;
+`;
+const Header = styled.header`
+    grid-area: header;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+    grid-area: main;
+`;
+const Nav = styled.nav`
+    grid-area: nav;
+`;
+const Aside = styled.aside`
+    grid-area: aside;
+`;
+const Footer = styled.footer`
+    grid-area: footer;
+`;
