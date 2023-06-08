8537aa50fbac53dbf5704576a5483dbab79809c2
wenyongqd <wenyongqd@gmail.com>
2023-06-08 09:57:53 -0400

    feat: implemented customized row component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,63 +3,34 @@ import { ProjectListScreen } from 'screens/project-list';
 import { useAuth } from 'context/auth-context';
 import { Button } from 'antd';
 import styled from '@emotion/styled';
+import { Row } from './components/lib';
 
 export const AuthenticatedApp = () => {
     const { logout } = useAuth();
     return (
         <Container>
-            <Header>
-                <HeaderLeft>
-                    <h3>Logo</h3>
-                    <h3>Projects</h3>
-                    <h3>User</h3>
+            <Header between={true}>
+                <HeaderLeft gap={true}>
+                    <h2>Logo</h2>
+                    <h2>Projects</h2>
+                    <h2>User</h2>
                 </HeaderLeft>
                 <HeaderRight>
                     <Button onClick={logout}>Log out</Button>
                 </HeaderRight>
             </Header>
-            <Nav>nav</Nav>
             <Main>
                 <ProjectListScreen />
             </Main>
-            <Aside>aside</Aside>
-            <Footer>footer</Footer>
         </Container>
     );
 };
-
 const Container = styled.div`
     display: grid;
     grid-template-rows: 6rem 1fr 6rem;
-    grid-template-columns: 20rem 1fr 20rem;
-    grid-template-areas:
-        'header header header'
-        'nav main aside'
-        'footer footer footer';
     height: 100vh;
-    grid-gap: 10rem;
 `;
-const Header = styled.header`
-    grid-area: header;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-    display: flex;
-    align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
-const Main = styled.main`
-    grid-area: main;
-`;
-const Nav = styled.nav`
-    grid-area: nav;
-`;
-const Aside = styled.aside`
-    grid-area: aside;
-`;
-const Footer = styled.footer`
-    grid-area: footer;
-`;
+const Main = styled.main``;
